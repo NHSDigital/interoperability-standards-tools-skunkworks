ef2cd375c3ebed3e7a4b92a39defcbc7eb7a60d9
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2025-01-22 09:48:55 +0000

    7.4.5 validator specific version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  validateUrl : 'https://ulg1llnfga.execute-api.eu-west-2.amazonaws.com/FHIR/R4',
+  validateUrl : 'https://ze03qz2v61.execute-api.eu-west-2.amazonaws.com/FHIR/R4',
   sdcServer : 'https://oh5vdeg59i.execute-api.eu-west-2.amazonaws.com/FHIR/R4',
   isDocker: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //validateUrl : 'https://3cdzg7kbj4.execute-api.eu-west-2.amazonaws.com/poc/Conformance/FHIR/R4',
-  validateUrl : 'https://ulg1llnfga.execute-api.eu-west-2.amazonaws.com/FHIR/R4',
+  validateUrl : 'https://ze03qz2v61.execute-api.eu-west-2.amazonaws.com/FHIR/R4',
   sdcServer : 'https://oh5vdeg59i.execute-api.eu-west-2.amazonaws.com/FHIR/R4',
   isDocker : false
 };
